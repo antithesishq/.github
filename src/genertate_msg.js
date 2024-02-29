24dec77deddae1ca900d38bfdcbbe1d171ee4039
Youhana Naseim <3439582+youhanaNaseim@users.noreply.github.com>
2024-02-29 14:57:22 -0500

    Simplify the action

--- src/genertate_msg.js
+++ src/genertate_msg.js
@@ -1,59 +1,14 @@
 module.exports = async ({github, context, core}) => {
-    // Get commit sha    
-    let commit_sha = context.sha
-
-    // Look up pull request by commit sha
-    const query = `query associatedPRs($sha: String, $repo: String!, $owner: String!){
-      repository(name: $repo, owner: $owner) {
-        commit: object(expression: $sha) {
-          ... on Commit {
-            associatedPullRequests(first:5){
-              edges{
-                node{
-                  title
-                  number
-                  body
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
-
-    const variables = {
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      sha: commit_sha
-    }
-    const result = await github.graphql(query, variables)
-
     let review_url = context.payload.compare
 
-    const pullRequests = result.repository.commit.associatedPullRequests.edges
-    if (pullRequests && pullRequests.length > 0) {
-
-      let pullrequest_id = pullRequests[0].node.number
-
-      // Retrieve pull request object & inspect approvers
-      const pullrequest_result = await github.request('GET /repos/{owner}/{repo}/pulls/{pull_number}',{
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        pull_number: pullrequest_id,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      })
-
-      if (pullrequest_result) {
-        review_url = pullrequest_result.data.html_url
-      }
-    }
-
-    // - brian.lagoda feature-proxy-restarts-on-fatals | f77c8a53de09 Merge with default
+    let message = `- ${context.actor} ${context.ref} | ${context.sha} [${context.payload.head_commit.message}](${review_url})`
+    let topic = context.repo.repo
 
     console.log(context)
 
-    core.setOutput("topic", context.repo.repo);
-    core.setOutput("msg", "The message from the JS script");
+    console.log(`Topic: ${topic}`)
+    console.log(`Msg: ${message}`)
+
+    core.setOutput("topic", topic);
+    core.setOutput("msg", message);
   }
