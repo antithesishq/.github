b6f7fe6a6d0c11150bd328bebde275077a2bee48
Youhana Naseim <3439582+youhanaNaseim@users.noreply.github.com>
2024-02-29 14:58:43 -0500

    Beautify the message

--- src/genertate_msg.js
+++ src/genertate_msg.js
@@ -1,10 +1,8 @@
 module.exports = async ({github, context, core}) => {
     let review_url = context.payload.compare
 
-    let message = `- ${context.actor} ${context.ref} | ${context.sha} [${context.payload.head_commit.message}](${review_url})`
+    let message = `- ${context.actor} ${context.ref} | ${context.sha.substring(0,7)} | [${context.payload.head_commit.message}](${review_url})`
     let topic = context.repo.repo
-
-    console.log(context)
 
     console.log(`Topic: ${topic}`)
     console.log(`Msg: ${message}`)
